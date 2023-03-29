61bbfe603af3fcfe1f2bc906768df5ab64935243
Gilad Carmel <giladcarmel22@gmail.com>
2023-03-29 19:33:57 +0300

    finished ex2

--- Exercises/Exercise-2/main.py
+++ Exercises/Exercise-2/main.py
@@ -1,11 +1,21 @@
 import requests
-import pandas
+import pandas as pd
 
+URL = "https://www.ncei.noaa.gov/data/local-climatological-data/access/2021/"
 
 
 def main():
-    # your code here
-    pass
+    df = pd.read_html(URL)[0]
+
+    filename = df[df["Last modified"] == "2022-02-07 14:03"]["Name"].min()
+
+    file_path = URL+filename
+
+    data = pd.read_csv(file_path)
+
+    max_hourly_temp = data[data["HourlyDryBulbTemperature"]
+                           == data["HourlyDryBulbTemperature"].max()]
+    print(max_hourly_temp)
 
 
 if __name__ == '__main__':
